1375c0d0d64bd440b9b912a9b4b1e9c1e76d6861
Koushik Dasika <kdasika@koushikdasika.com>
2012-10-19 19:49:03 -0400

    yay

--- problem28.java
+++ problem28.java
@@ -1,11 +1,32 @@
 public class problem28{
   public static void main(String[] args){
-    int numbers[][] = new int[5][5];
-    //int numbers[][] = new int[1001][1001];
-
-    int counter = numbers.length * numbers[1].length;
-    int n = numbers.length;
+    //int length = 5;
     int length = 1001;
+
+    int[][] numbers = createArray(length);
+    int sum = 0;
+
+    for(int i = 0; i < length; i++){
+      sum += numbers[i][i];
+      sum += numbers[length-i-1][i];
+    }
+    System.out.println(sum-1);
+
+    /*
+    for(int i = 0; i < numbers.length; i++){
+      for(int j = 0; j < numbers[i].length; j++){
+        System.out.print(numbers[i][j] + " ");
+      }
+      System.out.println();
+    }
+    */
+  }
+
+  public static int[][] createArray(int n){
+    int numbers[][] = new int[n][n];
+
+    int counter = n*n;
+    int length = n;
 
     for(int i = 0; i < (n/2)+1; i++){
       for(int j = n-i-1; j > i-1; j--){
@@ -27,14 +48,7 @@ public class problem28{
         numbers[j][n-i-1] = counter;
         counter--;
       }
-      
     }
-
-    for(int i = 0; i < numbers.length; i++){
-      for(int j = 0; j < numbers[i].length; j++){
-        System.out.print(numbers[i][j] + " ");
-      }
-      System.out.println();
-    }
+    return numbers;
   }
 }
